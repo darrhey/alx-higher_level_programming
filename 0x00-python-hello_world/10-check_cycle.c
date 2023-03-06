d64d9d2105e530fa1a754103540220d1368a5f35
Elegbede Oludare <elegbededare@gmail.com>
2023-03-06 23:13:40 +0100

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if a singly linked list has a cycle
+* @list: points to a struct
+*
+* Return: 0 if no cycle, 1 if there is cycle
+*/
+int check_cycle(listint_t *list)
+{
+
+}
