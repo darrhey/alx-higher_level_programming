3ba5ae93c16f06ecf1fcb9e899dffd4e6812f19d
Elegbede Oludare <elegbededare@gmail.com>
2023-03-07 19:39:18 +0100

    inventing is a combination of brains and material

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(1,10):
+    for j in range(1, 10):
         if i == j:
             continue
         elif j < i:
             continue
-        elif i==8 and j==9:
+        elif i == 8 and j == 9:
             print("{}{}".format(i, j))
         print("{}{}".format(i, j), end=', ')
