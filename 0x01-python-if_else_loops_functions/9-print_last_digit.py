c08b78ce634521ff0a4171931830bc2ca728d9a1
Elegbede Oludare <elegbededare@gmail.com>
2023-03-07 20:37:11 +0100

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = (number % 10)
+    elif number < 0:
+        last_digit = (abs(number) % 10)
+    return last_digit
