500e0005843ed3d70d71ef0767887545eed788b8
Elegbede Oludare <elegbededare@gmail.com>
2023-03-07 17:03:36 +0100

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        i = '0'+i
-    print("{}".format(i), end=', ')
     if i == 99:
         print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end=', ')
