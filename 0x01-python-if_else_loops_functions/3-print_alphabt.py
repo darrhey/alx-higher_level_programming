9befe13310dae39228544821ec880a5faf613507
Elegbede Oludare <elegbededare@gmail.com>
2023-03-07 16:38:48 +0100

    alphabet without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i not 101 or i not 113:
-        print("{}".format(chr(i)), end='')
+    if i == 101 or i != 113:
+        continue;
+    print("{}".format(chr(i)), end='')
