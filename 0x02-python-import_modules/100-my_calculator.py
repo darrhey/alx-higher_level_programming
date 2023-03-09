b8f4187a8007b822302ebc6756b13cc0e85a3c2e
Elegbede Oludare <elegbededare@gmail.com>
2023-03-09 21:01:34 +0100

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
     if count != 3:
         print("Usage: .100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif av[1] not in op:
+    elif sys.argv[2] not in op:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = int(av[0])
-        b = int(av[2])
-        if av[1] is "+":
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif av[1] is "-":
+        elif sys.argv[2] is "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif av[1] is "*":
+        elif sys.argv[2] is "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif av[1] is "/":
+        elif sys.argv[2] is "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
