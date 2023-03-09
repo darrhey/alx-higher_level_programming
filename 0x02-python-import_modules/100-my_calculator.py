4815ee74f95bb5eb020d4e200f920ac6fdf12dd0
Elegbede Oludare <elegbededare@gmail.com>
2023-03-09 21:15:19 +0100

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
     av = sys.argv[1:]
     op = ["+", "-", "*", "/"]
     count = len(av)
-    if count is not 3:
+    if count != 3:
         print("Usage: .100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif sys.argv[2] not in op:
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if sys.argv[2] is "+":
+        if sys.argv[2] == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
+        elif sys.argv[2] == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
+        elif sys.argv[2] == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
+        elif sys.argv[2] == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
