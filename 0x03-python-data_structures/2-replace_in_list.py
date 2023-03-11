dad31e68fb8e6d32468bd8c11aebd89949548e6e
Elegbede Oludare <elegbededare@gmail.com>
2023-03-11 14:20:46 +0100

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
    if idx < 0:
-       return my_list
        continue
    elif idx >= len(my_list):
-       return my_list
        continue
    else:
        my_list[idx] = element
-       return my_list
+   return my_list
