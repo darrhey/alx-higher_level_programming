220112cb279fe9e10892c5f96363c600b29f4fd6
Elegbede Oludare <elegbededare@gmail.com>
2023-03-12 10:27:41 +0100

    can you c me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-   while my_string.find('c' or 'C') != -1:
-      my_string.pop(my_string.find("c" or "C"))
+   while my_string.find("c") != -1:
+      my_string.pop(my_string.find("c"))
+   while my_string.find("C") != -1:
+      my_string.pop(my_string.find("C"))
    return my_list
