9e2e97f282424f1134a7ce3c4552d7b6acf32126
Elegbede Oludare <elegbededare@gmail.com>
2023-03-15 15:56:36 +0100

    complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,4 @@ def complex_delete(a_dictionary, value):
        for key, values in a_dictionary:
            if values == value:
                del a_dictionary[key]
-       return a_dictionary
+   return a_dictionary
