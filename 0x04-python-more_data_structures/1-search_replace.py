3e8a77c580dff0a4078bac3d67611e85c0f559c4
Elegbede Oludare <elegbededare@gmail.com>
2023-03-15 16:12:56 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-   new = my_list.copy()
-   i = 0
-   for j in new:
-       if j == search:
-           new.pop([i])
-           new.insert(i, replace)
-           i += 1
-       else:
-           i += 1
-   return new
+    new = my_list.copy()
+    i = 0
+        for j in new:
+            if j == search:
+                new.pop([i])
+                new.insert(i, replace)
+                i += 1
+            else:
+                i += 1
+    return new
