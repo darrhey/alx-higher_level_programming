44db455405e8ac1bbff1266e378cdaa54590b376
Elegbede Oludare <elegbededare@gmail.com>
2023-03-28 22:48:06 +0100

    Python classes corrected

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -20,14 +20,27 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
 
 
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print('#', end='')
-                print()
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
