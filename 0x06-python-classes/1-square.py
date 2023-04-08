44573d2d16ca71b1ac2b5c6a83ea4731d295d3cf
Elegbede Oludare <elegbededare@gmail.com>
2023-04-08 10:53:33 +0100

    square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Squsre generation module for 0x06
+"""
 class Square:
+    """class defined for square generation
+"""
     def __init__(self, size):
         self.__size = size
