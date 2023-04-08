78fc151a39ec11e9cf5ab7b20c59f5a805890e48
Elegbede Oludare <elegbededare@gmail.com>
2023-04-08 11:23:05 +0100

    documentation added

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,6 +2,26 @@
 """Square generation module for 0x06
 """
 class Square:
+    """class defined for square generation
+Args:
+size(int): length of one side of square
+Attributees:
+__size(int): length of one side of square
+Raises:
+TypeError: if size is not an integer
+ValueError: if size is less than 0
+"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        
+    def area(self):
+        return self.__size**2
+
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.position = position
